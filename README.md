d5962540422ee7a6efcc51c7615fb98510b25aa7
yky <yky@iquest.co.kr>
2018-07-19 12:32:14 +0900

    카카오 챗봇 app

---
+++ README.md
@@ -0,0 +1,19 @@
+# guide blog url : [이동](http://codevkr.tistory.com/31?category=719250)
+
+# KakaoChatBot
+
+
+## Usage
+
+
+
+## Developing
+
+
+
+### Tools
+
+Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
+ ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
+
+Nodeclipse is free open-source project that grows with your contributions.
---
+++ app.js
@@ -0,0 +1,25 @@
+//express 모듈 불러온 후 express 변수에 저장
+var express = require('express');
+
+//http 모듈 불러온 후 http 변수에 저장
+var http = require('http');
+
+//express 객체를 app 변수에 저장
+var app = express();
+
+//http://서버주소/keyboard
+app.get('/keyboard' , function(req, res){
+	//전달할 데이터
+	var data = {
+		'type' : 'buttons',
+		'buttons' : ['과일','채소','정보']
+	};
+	
+	//JSON 형식으로 응답
+	res.json(data);
+});
+
+//9090포트로 서버 실행
+http.createServer(app).listen(9090, function() {
+	console.log('9090 port Server Startup..');
+})
